7b29a9454e74b19717e1673465320490cb0e4cff
chendianzhang <chen_dian_zhang@sina.com>
2020-02-09 18:29:57 +0800

    fix not show picture bug

--- Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
+++ Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import h5py
 import scipy
+import pylab
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
@@ -13,4 +14,5 @@ if __name__ == "__main__":
     # Example of a picture
     index = 25
     plt.imshow(train_set_x_orig[index])
+    pylab.show()
     print("y = " + str(train_set_y[:, index]) + ", it's a " + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
