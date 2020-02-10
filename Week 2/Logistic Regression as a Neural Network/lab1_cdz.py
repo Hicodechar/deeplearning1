0a3cefe27f4d45c07696b88dab6c2ece3c0364fe
chendianzhang <chen_dian_zhang@sina.com>
2020-02-10 20:34:47 +0800

    finish 4: Building the parts of our algorithm

--- Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
+++ Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
@@ -98,6 +98,104 @@ def propagate(w, b, X, Y):
     
     return grads, cost
 
+# GRADED FUNCTION: optimize
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+    
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ### 
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w - dw * learning_rate
+        b = b - db * learning_rate
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+# GRADED FUNCTION: predict
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    ### START CODE HERE ### (≈ 1 line of code)
+    A = sigmoid(np.dot(w.T, X) + b)
+    ### END CODE HERE ###
+    
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        ### START CODE HERE ### (≈ 4 lines of code)
+        if(A[0, i]>0.5) :
+            Y_prediction[0, i] = 1
+        else:
+            Y_prediction[0, i] = 0
+        ### END CODE HERE ###
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
 if __name__ == "__main__":
     # Loading the data (cat/non-cat)
     train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
@@ -152,3 +250,17 @@ if __name__ == "__main__":
     print ("dw = " + str(grads["dw"]))
     print ("db = " + str(grads["db"]))
     print ("cost = " + str(cost))
+
+
+    params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+    print ("w = " + str(params["w"]))
+    print ("b = " + str(params["b"]))
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+
+
+    w = np.array([[0.1124579],[0.23106775]])
+    b = -0.3
+    X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+    print ("predictions = " + str(predict(w, b, X)))
