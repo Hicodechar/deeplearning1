88d8d2df9353ff04357e1a6406f6b73b2cbc6507
chendianzhang <chen_dian_zhang@sina.com>
2020-02-10 16:54:22 +0800

    finish not optimize version:Forward and Backward propagation.

--- Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
+++ Week 2/Logistic Regression as a Neural Network/lab1_cdz.py
@@ -7,6 +7,97 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
+
+# GRADED FUNCTION: sigmoid
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1/(1+np.exp(-z))
+    ### END CODE HERE ###
+    
+    return s
+
+
+# GRADED FUNCTION: initialize_with_zeros
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    w = np.zeros((dim, 1))
+    b = 0
+    ### END CODE HERE ###
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+# GRADED FUNCTION: propagate
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T, X) + b)                                    # compute activation
+    cost = -(np.dot((Y*np.log(A) + (-Y+1) * np.log(-A+1)), np.ones((m,1)))/m)                                # compute cost
+    print(cost)
+    ### END CODE HERE ###
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dw = np.dot((A - Y), X.T).T/m
+    db = np.dot((A - Y), np.ones((m, 1)))/m
+    db = np.squeeze(db)
+    print(dw)
+    print(db)
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
 if __name__ == "__main__":
     # Loading the data (cat/non-cat)
     train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
@@ -14,7 +105,7 @@ if __name__ == "__main__":
     # Example of a picture
     index = 25
     plt.imshow(train_set_x_orig[index])
-    pylab.show()
+    # pylab.show()
     print("y = " + str(train_set_y[:, index]) + ", it's a " + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
 
     ### START CODE HERE ### (≈ 3 lines of code)
@@ -47,3 +138,17 @@ if __name__ == "__main__":
 
     train_set_x = train_set_x_flatten/255
     test_set_x = test_set_x_flatten/255
+
+    print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+
+    dim = 2
+    w, b = initialize_with_zeros(dim)
+    print ("w = " + str(w))
+    print ("b = " + str(b))
+
+
+    w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+    grads, cost = propagate(w, b, X, Y)
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+    print ("cost = " + str(cost))
